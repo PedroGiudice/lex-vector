efa4aa7a37d62c256c95d94cee3861260e7108a6
PedroGiudice <pedrogr_gr@hotmail.com>
2026-02-27 05:15:14 +0100

    Update embed_hybrid.py

--- stj-vec/modal/embed_hybrid.py
+++ stj-vec/modal/embed_hybrid.py
@@ -37,7 +37,7 @@ image = modal.Image.debian_slim(python_version="3.11").pip_install(
     },
     timeout=3600,
     scaledown_window=120,
-    max_containers=4,
+    max_containers=8,
 )
 class HybridEmbedder:
     @modal.enter()
